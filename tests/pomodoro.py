8d24a2cb86ba3e0c2e46f787442ec570111596fa
Luciana Sampaio <luciana.sampaio84@gmail.com>
2026-01-24 13:08:05 -0300

    Atualizado 24/01/26

---
+++ tests/pomodoro.py
@@ -0,0 +1,5 @@
+import time
+time.sleep(25 * 60)
+while True:
+    print("acabou o tempo, va descansar!!!!")
+    break
---
+++ validacao/02.Conversao.py
@@ -0,0 +1,22 @@
+# Exemplo de cálculo da taxa de conversão
+numero_conversoes = 50
+numero_visitantes = 1000
+
+taxa_conversao = (numero_conversoes / numero_visitantes) * 100
+print(f'Taxa de Conversão: {taxa_conversao:.2f}%')
+
+
+# Exemplo de cálculo do custo por conversão
+custo_total = 5000
+numero_conversoes = 50
+
+custo_por_conversao = custo_total / numero_conversoes
+print(f'Custo por Conversão: R${custo_por_conversao:.2f}')
+
+# Exemplo de cálculo do valor médio por conversão
+receita_total = 10000
+numero_conversoes = 50
+
+valor_medio_por_conversao = receita_total / numero_conversoes
+print(f'Valor Médio por Conversão: R${valor_medio_por_conversao:.2f}')
+
